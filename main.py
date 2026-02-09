2d06c5fa82b470509f1e1152dfbfd24122748a6b
keerthanahrkeerthana18-lgtm <keerthanahrkeerthana18@gmail.com>
2026-02-09 13:06:49 +0530

    ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes.user_routes import router as user_router
+from routes.ai_response_routes import router as ai_response_router
 from db import get_db,DATABASE_URL
 from sqlalchemy import create_engine
 import os
@@ -8,6 +9,7 @@ from models import Base
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(ai_response_router)
 
 
 engine = create_engine(DATABASE_URL)
